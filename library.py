0bb630768aa8dfb133121e78bc6aaaaa5825e6dd
Mirko Manalo <mirko.manalo@gmail.com>
2025-12-14 11:58:26 +0100

    Feature: return_book aggiunto

--- library.py
+++ library.py
@@ -49,6 +49,27 @@ class Library:
             return f"Error: {found_user.name} can't loan book anymore. Loan limit is: {found_user.loan_limit}"
         return f"Error: book is already loaned"
     
+    def return_book(self, isbn, user_id):
+        found_book = self.find_book(isbn)
+        if found_book == "Error: Book not found":
+            return "Error: Book not found"
+        found_user = self.find_user(user_id)
+        if found_user == "Error: User not found":
+            return "Error: User not found"
+        
+        if not found_book.is_loaned:
+            return "Error: Book isn't loaned"
+
+        if found_book in found_user.loaned_books:
+            found_user.loaned_books.remove(found_book)
+            found_book.is_loaned = False
+            return f"Book successfully returned to library"
+        return f"Error: Book isn't loaned to user: {found_user.name}"
+    
+
+
+
+    
         
 
 
